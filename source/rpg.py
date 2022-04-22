c4529ed79e12afeb6cc8319a934c2f2813eef06f
MukundaRajput <97859392+MukundaRajput@users.noreply.github.com>
2022-04-21 20:39:00 -0400

    Removed the contact details and altered vocabulary
    Deleted the contact details of the source code contributor which I don't want to show for this project as it is an open source. I also updated the variable name from size to length for easy reading.

--- source/rpg.py
+++ source/rpg.py
@@ -8,20 +8,11 @@ def random_password_generator():
 	# Add character strings to chars variable with the strings showing uppercase, lowercase, numbers, and special characters.
 	# string.ascii_letters is a chain of the ascii_lowercase and ascii_uppercase 
     chars = string.ascii_letters + string.digits + string.punctuation
-    size = 4
-    return ''.join(random.choice(chars) for x in range(size, 20))
+    length = 4
+    return ''.join(random.choice(chars) for x in range(length, 20))
 
 def random_password_generator_ico():
 	random_password_generator_ico = """
-	#############################################################
-	# PYTHON - Random Password Generetor (RPG) - GH0ST S0FTWARE #
-	#############################################################
-	#                         CONTACT                           #
-	#############################################################
-	#               DEVELOPER : İSMAİL TAŞDELEN                 #
-	#          Mail Address : pentestdatabase@gmail.com         #
-	#   LINKEDIN : https://www.linkedin.com/in/ismailtasdelen   #
-	#              Whatsapp : + 90 534 295 94 31                #
-	#############################################################
+
 	"""
 	print(random_password_generator_ico)
