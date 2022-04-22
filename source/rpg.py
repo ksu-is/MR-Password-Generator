4b85a545d9272ca51eef2fb04bff45032f2b81cc
MukundaRajput <97859392+MukundaRajput@users.noreply.github.com>
2022-04-21 21:00:01 -0400

    Tried to add TKinter for GUI based window
    Tried to add a code to use GUI based window to select the length of the password. Will work on this more to introduce into my project.

--- source/rpg.py
+++ source/rpg.py
@@ -3,6 +3,7 @@
 
 import string
 import random
+#from tkinter import IntVar, StringVar
 
 def random_password_generator():
 	# Add character strings to chars variable with the strings showing uppercase, lowercase, numbers, and special characters.
@@ -10,6 +11,16 @@ def random_password_generator():
     chars = string.ascii_letters + string.digits + string.punctuation
     length = 4
     return ''.join(random.choice(chars) for x in range(length, 20))
+
+# pass_len = IntVar()
+# pass_str = StringVar()
+# def Generator():
+#    password = ''
+#    for x in range (0,4):
+#        password = random.choice(string.ascii_uppercase) + random.choice(string.ascii_lowercase) + random.choice(string.digits) + random.choice(string.punctuation)
+#    for y in range(pass_len.get()- 4):
+#        password = password + random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation)
+#    pass_str.set(password)
 
 def random_password_generator_ico():
 	random_password_generator_ico = """
