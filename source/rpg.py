615126eeeec844b727d68225fb29b66bc69c7520
MukundaRajput <97859392+MukundaRajput@users.noreply.github.com>
2022-04-21 20:02:16 -0400

    Added a comment line
    adding comments as I make the changes.

--- source/rpg.py
+++ source/rpg.py
@@ -5,6 +5,7 @@ import string
 import random
 
 def random_password_generator():
+	# Add character strings to chars variable with the strings showing uppercase, lowercase, numbers, and special characters.
     chars = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
     size = 4
     return ''.join(random.choice(chars) for x in range(size, 20))
